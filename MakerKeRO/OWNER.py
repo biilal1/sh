266d671a3822528e6e5ca6789dcdc49387970ed7
biilal1 <135733490+biilal1@users.noreply.github.com>
2024-10-19 14:18:01 +0300

    Update OWNER.py

--- MakerKeRO/OWNER.py
+++ MakerKeRO/OWNER.py
@@ -7,14 +7,14 @@ from pyrogram import filters
 load_dotenv()
 
 
-OWNER = ["Y_J_J_J","H_H_H_P","N_7_K"]
-KeroId = 6920192910
-OWNER_NAME = "𓏺-َِ𝑲َِ𝑬َِ𝑹َِ𝑶Ꮠ͋͢➢𝙀َِ𝘭َِ𝙃َِ𝘢َِ𝙆َِ𝘦َِ𝙈⤸"
-BOT_TOKEN = "7529604109:AAESIvco6lcEbVrCaXNv3mJyvmUsk5FPOfA"
+OWNER = ["BDB0BJ","BDB0B","BDB0B"]
+KeroId = 7291869416
+OWNER_NAME = "- Me BiLaL </>"
+BOT_TOKEN = "7064480810:AAHZn4ID-aUs6yaxA6XU-JBl8syJxPHLAZM"
 DATABASE = "mongodb+srv://DevKERO:DevKERO123@kero.ljshi.mongodb.net/?retryWrites=true&w=majority&appName=KERO"
-CHANNEL = "https://t.me/OE_YT"
-GROUP = "https://t.me/UF_CV"
-VID_SO = "https://t.me/CACASF4235/3"
-VIDEO = "https://t.me/CACASF4235/3"
-PHOTO = "https://raw.githubusercontent.com/KeroIsDev/keroooo/refs/heads/main/IMG_20240921_201153_186.jpg"
+CHANNEL = "https://t.me/o_p_g"
+GROUP = "https://t.me/qu_quu"
+VID_SO = "https://t.me/CACASF4235/4"
+VIDEO = "https://t.me/CACASF4235/4"
+PHOTO = "https://te.legra.ph/file/f7e1acf9338cc453a87ad.jpg""
 LOGS = "Y_J_J_J"
